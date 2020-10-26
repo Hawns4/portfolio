659f9fc87577ecc5088bcb5e197e567577a823c5
Hans Cabrera <hanscabrera04@gmail.com>
2020-10-26 01:08:07 -0600

    css reset

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
 
 export const Projects = () => (
     <Container>
-        <h1>Projects</h1>
+        <Jumbotron>
+            <h1>Projects</h1>
+            <h3>My Work</h3>
+        </Jumbotron>
     </Container>
 )
